4789d5d0fbc71d48f3f358c9ee492311e7a81e72
stephie-spec <compwork254@gmail.com>
2025-10-18 22:26:54 +0300

    adds routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then(r => r.json())
+      .then(setMovies)
+      .catch(console.error);
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </main>
     </>
   );
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then(r => r.json())
+      .then(setMovie)
+      .catch(console.error);
+  }, [id]);
+
+  if (!movie) return <h1>Loading...</h1>;
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>Time: {movie.time} minutes</p>
+        <div>
+          {movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
       </main>
     </>
   );
